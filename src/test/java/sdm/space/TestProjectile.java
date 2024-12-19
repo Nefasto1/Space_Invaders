332192ea22e89d5baf4ab8cc34c571352216c61d
Nefasto1 <stefano.tuminost@gmail.com>
2024-12-19 14:53:51 +0100

    Added new test for Projectile movement

--- src/test/java/sdm/space/TestProjectile.java
+++ src/test/java/sdm/space/TestProjectile.java
@@ -3,18 +3,32 @@ package sdm.space;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestProjectile {
     @Test
-    public void moveUpFrom() {
+    public void moveFromTwoAndFive() {
         int x = 2;
         int y = 5;
 
         Projectile projectile = new Projectile(x, y);
         projectile.move();
         assertAll("Check of all the coordinates",
-                () -> projectile.getXPosition(),
-                () -> projectile.getYPosition()
+                () -> assertEquals(2, projectile.getXPosition()),
+                () -> assertEquals(10, projectile.getYPosition())
+        );
+    }
+
+    @Test
+    public void moveFromFiveAndSeven() {
+        int x = 5;
+        int y = 7;
+
+        Projectile projectile = new Projectile(x, y);
+        projectile.move();
+        assertAll("Check of all the coordinates",
+                () -> assertEquals(5, projectile.getXPosition()),
+                () -> assertEquals(12, projectile.getYPosition())
         );
 
     }
