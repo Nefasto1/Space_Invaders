ff3ea35abeb7eaa268c3cdee597c9286caaf3dd5
gaiiaalessio <gaiuzza24@hotmail.it>
2024-12-23 17:18:30 +0100

    added test moveRightFromEightAndTen for Alien

--- src/test/java/sdm/space/TestAlien.java
+++ src/test/java/sdm/space/TestAlien.java
@@ -17,4 +17,18 @@ public class TestAlien {
                 () -> assertEquals(5, alien.getYPosition())
         );
     }
+
+    @Test
+    public void moveRightFromEightAndTen(){
+        int x = 8;
+        int y = 10;
+
+        Alien alien = new Alien(x,y);
+        alien.moveRight();
+        assertAll("Check all the coordinates",
+                () -> assertEquals(10, alien.getXPosition()),
+                () -> assertEquals(10, alien.getYPosition())
+        );
+    }
+
 }
