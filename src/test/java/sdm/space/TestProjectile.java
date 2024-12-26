0c66141a2bb086a6e21ad58ad22128448ecec347
Nefasto1 <stefano.tuminost@gmail.com>
2024-12-26 10:46:05 +0100

    Added new limited movement test for projectile

--- src/test/java/sdm/space/TestProjectile.java
+++ src/test/java/sdm/space/TestProjectile.java
@@ -45,6 +45,19 @@ public class TestProjectile {
     }
 
     @Test
+    public void movementLimitedShifted() {
+        int x = 5;
+        int y = 496;
+
+        Projectile projectile = new Projectile(x, y);
+        projectile.move();
+        assertAll("Check of all the coordinates",
+                () -> assertEquals(5, projectile.getXPosition()),
+                () -> assertEquals(500, projectile.getYPosition())
+        );
+    }
+
+    @Test
     public void checkHasCollidedDefault() {
         Projectile projectile = new Projectile();
         assertFalse(projectile.hasCollided());
