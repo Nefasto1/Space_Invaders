f0d5cc723256eb7bf6441bf96e7547afc75f5a10
Nefasto1 <stefano.tuminost@gmail.com>
2025-01-02 17:30:53 +0100

    Fixed AlienFactory instantiation

--- src/main/java/sdm/gui/AlienFactory.java
+++ src/main/java/sdm/gui/AlienFactory.java
@@ -8,14 +8,8 @@ import java.util.stream.IntStream;
 
 public class AlienFactory {
     public static ArrayList<Alien> generate(int rows){
-//        ArrayList<Alien> aliens = new ArrayList<>();
-//        for (int i = 0; i < rows; i++) {
-//            aliens.add(new Alien(15 + i%5 * 40, 5 + i/5 * 40, "alien.png", 32, 32));
-//            aliens.add(new Alien(15 + i%5 * 40, 5 + i/5 * 40));
-//        }
-
         return IntStream.range(0, rows*5)
-                .mapToObj(i -> new Alien(15 + i%5 *40, 5 + i/5 *40))
+                .mapToObj(i -> new Alien(15 + i%5 *40, 5 + i/5 *40, 45, 45, "alien.png"))
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 }
