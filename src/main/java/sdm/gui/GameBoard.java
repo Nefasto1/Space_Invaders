084b8298ea33d856d9067acaacbfad7cc825a6aa
Nefasto1 <stefano.tuminost@gmail.com>
2025-01-02 17:42:43 +0100

    Added the GameMenu to GameBoard

--- src/main/java/sdm/gui/GameBoard.java
+++ src/main/java/sdm/gui/GameBoard.java
@@ -2,11 +2,13 @@ package sdm.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GameBoard extends JPanel {
     private StartMenu startMenu;
     private EndMenu endMenu;
-//    private GameMenu gameMenu;
+    private GameMenu gameMenu;
     private GameManager game;
 
     private String state = "Start";
@@ -16,6 +18,7 @@ public class GameBoard extends JPanel {
     }
 
     private void initializeBoard() {
+        addKeyListener(new TAdapter());
         setFocusable(true);
 
         int width = 500;
@@ -26,8 +29,9 @@ public class GameBoard extends JPanel {
 
         startMenu = new StartMenu(this);
         endMenu = new EndMenu(this);
-        game = new GameManager();
+        gameMenu = new GameMenu(this);
 
+        game = new GameManager();
     }
 
     @Override
@@ -45,11 +49,8 @@ public class GameBoard extends JPanel {
         if (state.equals("Start"))
             drawStartMenu(g2d);
 
-        System.out.println("Start");
-
         if (state.equals("Game"))
-            // To draw Game
-            System.out.println("Game");
+            drawGame(g2d);
 
         if (state.equals("End"))
             drawEndMenu(g2d);
@@ -63,8 +64,19 @@ public class GameBoard extends JPanel {
 
         startMenu.draw(g2d, width, height);
         if (startMenu.isStart()) {
-            game.reset();
             state = "Game";
+            game.reset();
+        }
+    }
+
+    private void drawGame(Graphics2D g2d) {
+        int width = getWidth();
+        int height = getHeight();
+
+        gameMenu.draw(g2d, width, height, game);
+        if (game.isEnded()) {
+            endMenu.reset();
+            state = "End";
         }
     }
 
@@ -76,6 +88,20 @@ public class GameBoard extends JPanel {
         if (endMenu.isRetry()) {
             game.reset();
             state = "Game";
+        }
+    }
+
+    private class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_LEFT)
+                game.moveLeftShuttle();
+            else if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+                game.moveRightShuttle();
+
+            if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_SPACE) {
+                game.shuttleShot();
+            }
         }
     }
 }
