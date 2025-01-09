876f6211d2f8c059553c24e4991126085b4a05c2
Nefasto1 <stefano.tuminost@gmail.com>
2025-01-09 16:19:29 +0100

    Forgot to push StartMenu :c

--- src/main/java/sdm/gui/StartMenu.java
+++ src/main/java/sdm/gui/StartMenu.java
@@ -3,18 +3,48 @@ package sdm.gui;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StartMenu {
     private JButton startButton;
     private JButton exitButton;
     private boolean start=false;
 
+    private JRadioButton[] barrierButtons;
+    private JRadioButton[] rowButtons;
+    private JRadioButton[] speedButtons;
+
+    private int selectedBarriers = 0;
+    private int selectedRows = 1;
+
     public StartMenu(JPanel panel) {
         startButton = createButton("Start", e -> {setVisible(false); start = true;});
         exitButton = createButton("Exit", e -> System.exit(0));
+        barrierButtons = CreateRadioButtons(panel, IntStream.range(0, 6).toArray(), e -> selectedBarriers = Integer.parseInt(((JRadioButton) e.getSource()).getText()));
+        rowButtons = CreateRadioButtons(panel, IntStream.range(1, 6).toArray(), e -> selectedRows = Integer.parseInt(((JRadioButton) e.getSource()).getText()));
 
         panel.add(startButton);
         panel.add(exitButton);
+    }
+
+    private JRadioButton[] CreateRadioButtons(JPanel panel, int[] values, ActionListener action) {
+        ButtonGroup group = new ButtonGroup();
+        JRadioButton[] buttons = new JRadioButton[values.length];
+
+        for (int i = 0; i < values.length; i++) {
+            buttons[i] = new JRadioButton(Integer.toString(values[i]));
+            buttons[i].addActionListener(action);
+            buttons[i].setBackground(new Color(0, 32, 48));
+            buttons[i].setForeground(Color.white);
+            group.add(buttons[i]);
+            panel.add(buttons[i]);
+        }
+        buttons[0].setSelected(true);
+
+        return buttons;
     }
 
     private JButton createButton(String text, ActionListener action) {
@@ -36,6 +66,8 @@ public class StartMenu {
     private void setVisible(boolean visible) {
         startButton.setVisible(visible);
         exitButton.setVisible(visible);
+        Arrays.stream(barrierButtons).forEach(button -> button.setVisible(visible));
+        Arrays.stream(rowButtons).forEach(button -> button.setVisible(visible));
     }
 
     public boolean isStart() {
@@ -46,8 +78,24 @@ public class StartMenu {
         drawBackground(g2d, width, height);
         drawTitle(g2d, width, height);
 
-        startButton.setBounds(50, height/2 - 75, width-100, 75);
-        exitButton.setBounds(50, height/2 + 25, width-100, 75);
+        drawRadioButtons(g2d, "Number of Barriers:", barrierButtons, width/8, height/6 + 225);
+        drawRadioButtons(g2d, "Number of Rows:", rowButtons, width/8, height/6 + 275);
+
+        startButton.setBounds(50, height/6, width-100, 75);
+        exitButton.setBounds(50, height/6 + 100, width-100, 75);
+    }
+
+    private void drawRadioButtons(Graphics2D g2d, String labelText, JRadioButton[] buttons, int x, int y) {
+        Font font = new Font("Helvetica", Font.PLAIN, 20);
+        g2d.setFont(font);
+        g2d.setColor(Color.white);
+
+        g2d.drawString(labelText, x, y - 10);
+
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setBounds(x + 150 + (i * 50), y, 20, 20);
+            g2d.drawString(buttons[i].getText(), x + 136 + (i * 50), y + 18);
+        }
     }
 
     private void drawBackground(Graphics2D g2d, int width, int height) {
@@ -61,6 +109,14 @@ public class StartMenu {
 
         g2d.setColor(Color.white);
         g2d.setFont(big);
-        g2d.drawString("Space Invaders", (width - bigText.stringWidth("Space Invaders")) / 2,height / 4);
+        g2d.drawString("Space Invaders", (width - bigText.stringWidth("Space Invaders")) / 2,height / 8);
+    }
+
+    public int getNumBarriers() {
+        return selectedBarriers;
+    }
+
+    public int getNumRows() {
+        return selectedRows;
     }
 }
